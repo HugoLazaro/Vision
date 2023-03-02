4dfa8c904168a3f5b40575cbeacf909196bbeb64
JorgeMG117 <jorgemartinezgil117@gmail.com>
2023-03-02 20:20:29 +0100

    gitignore added :)

--- prac1/efectos/distorsion.py
+++ prac1/efectos/distorsion.py
@@ -22,15 +22,26 @@ def apply(image, k1, k2):
 
     new_img = np.zeros((fy,fx,3), np.uint8)
     print("New image size: " + str(new_img.shape[1]) + ", " + str(new_img.shape[0]))
+    i = 0
 
     for yd in range(fy):
         for xd in range(fx):
             r2 = pow((xd - xcen),2) + pow((yd - ycen),2)
             xu = xd + (xd - xcen)* k1 * r2 + (xd - xcen)* k2 * pow(r2,2)
             yu = yd + (yd - ycen)* k1 * r2 + (yd - ycen)* k2 * pow(r2,2)
-            if xu < fx and yu < fy and xu > 0 and yu > 0:
-                # print(str(xu))
-                # print(str(yu))
-                new_img[xd][yd] = image[int(xu)][int(yu)]
+            xu = int(np.round(xu))
+            yu = int(np.round(yu))
+            
+            if float(xu) < fy and yu < fy and xu > 0 and yu > 0:
+                try:
+                    new_img[xd][yd] = image[xu][yu]
+                except:
+                    i = i+1
+                    # print("i: " + str(i))
+                    # print("fx: " + str(fx))
+                    # print("fy: " + str(fy))
+                    print("xu: " + str(xu))
+                    print("yu: " + str(yu))
+                #new_img[xd][yd] = image[xu][yu]
 
     return new_img
