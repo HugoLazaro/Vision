a34a358d6c64b4cfec5cc66c1d6e3fa55604bdd5
JorgeMG117 <jorgemartinezgil117@gmail.com>
2023-05-02 19:19:41 +0200

    aaaaaaa
    Co-authored-by: Hugo Lazaro <HugoLazaro@users.noreply.github.com>

--- prac4/practica5.py
+++ prac4/practica5.py
@@ -9,36 +9,54 @@ MIN_MATCH_COUNT = 10
 def ransac(matches, kp1, kp2):
     src_pts = np.float32([ kp1[m.queryIdx].pt for m in matches ]).reshape(-1,1,2)
     dst_pts = np.float32([ kp2[m.trainIdx].pt for m in matches ]).reshape(-1,1,2)
-    
+
     src_pts_list = np.zeros((4, 2))
     dst_pts_list = np.zeros((4, 2))
-    
+
     best_distance = np.Infinity
-    best_M, best_Mask = cv.findHomography(src_pts_list, dst_pts_list, 0,5.0)  
-    for iter in range(10):
+    best_M, best_Mask = cv.findHomography(src_pts_list, dst_pts_list, 0,5.0)
+    for iter in range(100):
         for i in range(4):
             num = random.randint(0, len(src_pts)-1)
             src_pts_list[i] = src_pts[num]
             dst_pts_list[i] = dst_pts[num]
 
-        
+
         M, mask = cv.findHomography(src_pts_list, dst_pts_list, 0,5.0)
-        
+        # print("Aqui")
+        # print(mask.shape)
+
 
         dst = cv.perspectiveTransform(src_pts, M)
+        #print(len(dst))
+        consensus = np.zeros((len(dst),1), dtype=int)
+        threshold_distance = 200
+        threshold_number = 50
+        max_number = 0
 
         distancias = np.sqrt(np.sum((dst - dst_pts)**2, axis=2))
+        for i in range(len(distancias)):
+            if distancias[i] < threshold_distance:
+                consensus[i] = 1
 
-        distancias = np.sum(distancias)
-        
-        if distancias < best_distance:
-            distancias = best_distance
+        if np.sum(consensus) > threshold_number:
             best_M = M
-            best_Mask = mask
+            best_Mask = consensus
+            # print(distancias)
+            return best_M, best_Mask
+        elif np.sum(consensus) > max_number:
+            best_M = M
+            best_Mask = consensus
+            max_number = len(consensus)
+
+        # if distancias < best_distance:
+        #     distancias = best_distance
+        #     best_M = M
+        #     best_Mask = mask
 
     return best_M, best_Mask
-    
-    
+
+
 
 
 
@@ -83,13 +101,36 @@ def main():
         if m.distance < 0.7*n.distance:
             good.append(m)
 
-   
-        
+
+
     if len(good)>MIN_MATCH_COUNT:
-        M, mask = ransac(good, kp1, kp2)
+        src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+        dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+        # print(M.shape)
+        # print(mask.shape)
+        # print(mask)
+
+        # M, mask = ransac(good, kp1, kp2)
+        # print(M.shape)
+        # print(mask.shape)
+        # #print(mask)
         matchesMask = mask.ravel().tolist()
-        h,w = img1.shape
+        h, w = img1.shape
+
+        img1_transformed = cv.warpPerspective(img1, M, (img2.shape[1], img2.shape[0]))
+
+        # img1_resized = cv.resize(img1, (img2.shape[1] + img1.shape[1], img2.shape[0] + img1.shape[0]))
+
+        # img2_resized = cv.resize(img2, (img2.shape[1] + img1.shape[1], img2.shape[0] + img1.shape[0]))
+        # # Juntar las dos imágenes
+        # result = cv.addWeighted(img2_resized, 0.5, img1_resized, 0.5, 0)
+
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+
+        result = cv.addWeighted(img2, 0.5, img1_transformed, 0.5, 0)
+        plt.imshow(result, 'gray'),plt.show()
+
         dst = cv.perspectiveTransform(pts,M)
         img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
     else:
@@ -102,7 +143,7 @@ def main():
                         flags = 2)
     img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
     plt.imshow(img3, 'gray'),plt.show()
- 
+
 
 if __name__ == "__main__":
     # Llamar a la función main()
